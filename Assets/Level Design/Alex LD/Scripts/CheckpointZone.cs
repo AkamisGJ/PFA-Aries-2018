d58a708a646d2b6c711aa1ce052cbdcd0e1040d6
BaberinosLeDouxTacos <39302412+BaberinosLeDouxTacos@users.noreply.github.com>
2018-05-15 15:50:49 +0200

    Alex Level Design
    C mon ld

---
+++ Assets/Level Design/Alex LD/Scripts/CheckpointZone.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointZone : MonoBehaviour {
+
+    public Transform pointToTP;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.transform.position = pointToTP.position;
+            other.transform.rotation = pointToTP.rotation;
+        }
+    }
+
+}
---
+++ Assets/Level Design/Alex LD/Scripts/OnTriggerTP.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnTriggerTP : MonoBehaviour
+{
+    private Animator animator;
+    public GameObject Target;
+
+    private void Awake()
+    {
+        animator = Target.GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag=="Player")
+        {
+            animator.SetBool("Turn", true);
+            Target.layer = LayerMask.NameToLayer("CanTeleport");
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            animator.SetBool("Turn", false);
+            Target.layer = LayerMask.NameToLayer("Default");
+        }
+    }
+}
