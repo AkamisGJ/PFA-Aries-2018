670dd62c87e46749d69b06af062107f50dfaf8df
BaberinosLeDouxTacos <39302412+BaberinosLeDouxTacos@users.noreply.github.com>
2018-05-23 14:36:05 +0200

    Commit pour pull

---
+++ Assets/Level Design/Alex LD/Scripts/BulletTime.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletTime : MonoBehaviour
+{
+
+    public float SlowMoFactor = 0.05f;
+    public float SlowMoLenght = 2f;
+
+
+    public Color color_Time;
+    private BoxCollider cubePosition;
+
+    void Start()
+    {
+        cubePosition = GetComponent<BoxCollider>();
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Time.timeScale = 0.2f;
+            Time.fixedDeltaTime = Time.timeScale * 0.02f;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Time.timeScale = 1;
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = color_Time;
+        cubePosition = GetComponent<BoxCollider>();
+        Gizmos.DrawCube(cubePosition.center + transform.position, transform.localScale);
+    }
+}
