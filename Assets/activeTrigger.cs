97eac3c6e60b5a5d55231b8a22fc2d0ba595d365
BaberinosLeDouxTacos <39302412+BaberinosLeDouxTacos@users.noreply.github.com>
2018-05-25 14:04:32 +0200

    Repair LD

--- Assets/activeTrigger.cs
+++ Assets/activeTrigger.cs
@@ -5,7 +5,12 @@ using UnityEngine;
 public class activeTrigger : MonoBehaviour {
 
 	public Eboulement_alex script;
+	public Eboulement_alex script2;
+
+	public Eboulement_alex script3;
 	void OnTriggerEnter(Collider other) {
 		script.active = true;
+		script2.active = true;
+		script3.active = true;
 	}
 }
