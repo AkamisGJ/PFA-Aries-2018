2d32134da04a1b8369a67930e5f0962dcafd6d50
BaberinosLeDouxTacos <39302412+BaberinosLeDouxTacos@users.noreply.github.com>
2018-05-25 09:35:10 +0200

    Work on scene 3

---
+++ Assets/Level Design/Alex LD/03 Scene/Eboulement_alex.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eboulement_alex : MonoBehaviour
+{
+    public int m_mass;
+    public bool active;
+
+    void Update()
+    {
+        if(active == true){
+        int nb_child = transform.GetChildCount();
+
+        for(int i = 0; i < nb_child; i++)
+        {
+            transform.GetChild(i).gameObject.AddComponent(typeof(Rigidbody));
+
+            transform.GetChild(i).GetComponent<Rigidbody>().useGravity = true;
+            transform.GetChild(i).GetComponent<Rigidbody>().mass = m_mass;
+
+
+        }
+        }
+        active = false;
+    }
+}
