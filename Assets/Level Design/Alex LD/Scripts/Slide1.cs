be30408e124d0ca7b5992323d4a9188b10b5e277
BaberinosLeDouxTacos <39302412+BaberinosLeDouxTacos@users.noreply.github.com>
2018-05-28 13:38:41 +0200

    Stairs

---
+++ Assets/Level Design/Alex LD/Scripts/Slide1.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slide1 : MonoBehaviour
+{
+    private Animator animator;
+    public GameObject Target;
+
+    private void Awake()
+    {
+        animator = Target.GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag=="Player")
+        {
+            animator.SetBool("Turn", true);
+            print("Lol");
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            animator.SetBool("Turn", true);
+            
+        }
+    }
+}
