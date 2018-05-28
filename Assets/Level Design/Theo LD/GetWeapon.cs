6e9bada0621ceb946eb768db1084b3acfdcb30f2
M0nk42 <theotbf@laposte.net>
2018-05-28 14:14:53 +0200

    alrs ya des escaliers et la fin de niveau et c'est mieux trié et puis tu peux prendre l'amre

---
+++ Assets/Level Design/Theo LD/GetWeapon.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetWeapon : MonoBehaviour {
+
+    public GameObject m_WeaponGet;
+    public GameObject m_WeaponPlayer;
+
+    void Start () {
+        m_WeaponPlayer.gameObject.SetActive(false);
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Debug.Log("magie !");
+            m_WeaponGet.gameObject.SetActive(false);
+            m_WeaponPlayer.gameObject.SetActive(true);
+        }
+
+    }
+}
