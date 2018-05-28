b5aae3a17ccda0e90d8bef323a90d3a391ff3fbc
benarof <39303386+benarof@users.noreply.github.com>
2018-05-28 13:10:28 +0200

    ben menu

--- Assets/Level Design/Ben LD/Script_Ben/Menu_Play.cs
+++ Assets/Level Design/Ben LD/Script_Ben/Menu_Play.cs
@@ -4,10 +4,50 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Menu_Play : MonoBehaviour 
+
+public class Menu_Play : MonoBehaviour
 {
+    public Animator camera;
+    
+
     public void startButton()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+
+    public void introduction()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Level1()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void Level2()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void Level3()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+    public void Level4()
+    {
+        SceneManager.LoadScene(5);
+    }
+
+    public void ToogleBool(string Parameter)
+    {
+        bool value = camera.GetBool(Parameter);
+        camera.SetBool(Parameter, !value);
     }
 }
