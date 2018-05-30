0fcd1189199891475fddec55de918a701fc6461d
BaberinosLeDouxTacos <39302412+BaberinosLeDouxTacos@users.noreply.github.com>
2018-05-30 15:52:07 +0200

    Sound Ready Scene Théo

---
+++ Assets/Level Design/Alex LD/Scripts/SoundTrigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundTrigger : MonoBehaviour 
+{
+
+	private BoxCollider TriggerZone;
+	private int OnceOnly =0;
+	public AudioSource m_audiosource;
+	public AudioClip explosion;
+
+	private void Start() 
+	{
+		TriggerZone = GetComponent<BoxCollider>();
+	}
+
+	private void OnTriggerEnter(Collider other) 
+	{
+		if (other.tag == "Player") 
+		{
+			print("Sound Is Playing");
+			m_audiosource.PlayOneShot(explosion);
+			OnceOnly += 1;
+			print (OnceOnly);
+			TriggerZone.enabled = false;
+		}
+	}
+
+}
