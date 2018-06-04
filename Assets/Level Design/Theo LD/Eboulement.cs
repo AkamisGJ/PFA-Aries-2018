1b948354034314e21416f7d4523f885873b4b2ca
M0nk42 <theotbf@laposte.net>
2018-06-04 16:05:45 +0200

    + de LD

--- Assets/Level Design/Theo LD/Eboulement.cs
+++ Assets/Level Design/Theo LD/Eboulement.cs
@@ -1,23 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using EZCameraShake;
 
 public class Eboulement : MonoBehaviour
 {
     public int m_mass;
+    public float timeShaking = 3f;
+    private GameObject m_Player;
+    private bool Tédjapassé = false;
+
+    private void Start()
+    {
+        m_Player = GameObject.FindGameObjectWithTag("Player");
+    }
 
     void OnTriggerEnter(Collider col)
     {
         int nb_child = transform.GetChildCount();
-
-        for(int i = 0; i < nb_child; i++)
+        if(Tédjapassé == false)
         {
-            transform.GetChild(i).gameObject.AddComponent(typeof(Rigidbody));
+            for (int i = 0; i < nb_child; i++)
+            {
+                Tédjapassé = true;
+                transform.GetChild(i).gameObject.AddComponent(typeof(Rigidbody));
 
-            transform.GetChild(i).GetComponent<Rigidbody>().useGravity = true;
-            transform.GetChild(i).GetComponent<Rigidbody>().mass = m_mass;
+                transform.GetChild(i).GetComponent<Rigidbody>().useGravity = true;
+                transform.GetChild(i).GetComponent<Rigidbody>().mass = m_mass;
 
-
+                CameraShakeInstance m_CameraPresset = new CameraShakeInstance(2f, 4f);
+                m_Player.GetComponentInChildren<CameraShaker>().ShakeOnce(m_CameraPresset.Magnitude, m_CameraPresset.Roughness, timeShaking, timeShaking);
+                
+            }
         }
+
     }
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
 }
